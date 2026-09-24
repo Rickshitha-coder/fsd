534f0b6f6839bc67c2c5c336e1774df550805b11
Rickshitha-coder <rickshithanandakumar@gmail.com>
2026-09-24 10:08:33 +0530

    Add new projects and assignments

---
+++ context-login/src/Navbar.js
@@ -0,0 +1,43 @@
+import {useContext} from "react";
+
+import {UserContext} from "./UserContext";
+
+
+function Navbar(){
+
+
+const {user}=useContext(UserContext);
+
+
+
+return(
+
+<div>
+
+
+<h3>
+
+{
+user ?
+
+"Welcome "+user.name
+
+:
+
+"Please Login"
+
+}
+
+
+</h3>
+
+
+</div>
+
+);
+
+
+}
+
+
+export default Navbar;
---
+++ login/script.js
@@ -0,0 +1,28 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let isValid = true;
+
+    document.getElementById("usererror").innerHTML = "";
+    document.getElementById("passworderror").innerHTML = "";
+
+    if (username == "") {
+        document.getElementById("usererror").innerHTML = "Username is required";
+        isValid = false;
+    }
+
+    if (password == "") {
+        document.getElementById("passworderror").innerHTML = "Password is required";
+        isValid = false;
+    } else if (password.length < 6) {
+        document.getElementById("passworderror").innerHTML = "Password must contain at least 6 characters";
+        isValid = false;
+    }
+
+    if (isValid) {
+        alert("Login successful");
+        window.location.href = "home.html";
+    }
+});
